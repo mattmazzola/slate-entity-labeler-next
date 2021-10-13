03502c3dbb1cd088482198080eb5b62518bcb0fa
Matt Mazzola <mattmazzola@live.com>
2021-10-13 16:25:10 -0700

    Update slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,176 @@
 import React from 'react'
 import './App.css'
+
 // Import the Slate editor factory.
-import { createEditor, Editor, Node } from 'slate'
+import { createEditor, Editor, BaseText, Text, Node, Transforms, BaseEditor } from 'slate'
 
 // Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
-import { CustomMarkProps } from 'slate-react/lib/components/custom'
+import { ReactEditor, Slate, Editable, withReact } from 'slate-react'
 
-type SlateValue = any[]
+type CustomElement = {
+  type: 'paragraph' | 'code'
+  children: CustomText[]
+}
 
-const defaultValue = [
-  {
-    type: 'paragraph',
-    children: [
-      {
-        text: 'A line of text in a paragraph.',
-        marks: [],
-      },
-    ],
-  },
-]
+type CustomText = BaseText & {
+  bold?: boolean
+}
+
+type CustomEditor = ReactEditor & BaseEditor
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: CustomEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
 
 // Define a serializing function that takes a value and returns a string.
-const serialize = (value: SlateValue) => {
+const serialize = (value: CustomElement[]) => {
   return (
     value
-      // Return the text content of each paragraph in the value's children.
-      .map(n => Node.text(n))
+      // Return the string content of each paragraph in the value's children.
+      .map(n => Node.string(n))
       // Join them all with line breaks denoting paragraphs.
       .join('\n')
   )
 }
 
+// Define a deserializing function that takes a string and returns a value.
+const deserialize = (value: string) => {
+  // Return a value array of children derived by splitting the string.
+  return value.split('\n')
+    .map(line => {
+      return {
+        children: [{ text: line }],
+      }
+    })
+}
+
+const initialValue: CustomElement[] = [
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: 'A line of text in a paragraph.'
+      },
+    ],
+  },
+]
+
+const CustomEditor = {
+  isBoldMarkActive(editor: CustomEditor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => (n as CustomText).bold === true,
+      universal: true,
+    })
+
+    return Boolean(match)
+  },
+
+  isCodeBlockActive(editor: CustomEditor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => (n as CustomElement).type === 'code',
+    })
+
+    return Boolean(match)
+  },
+
+  toggleBoldMark(editor: CustomEditor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true } as CustomText,
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+
+  toggleCodeBlock(editor: CustomEditor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' } as CustomElement,
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  },
+}
+
+
 const App: React.FC = () => {
-  const editor = React.useMemo(() => withReact(createEditor()), [])
-
-  // Define a rendering function based on the element passed to `props`. We use
-  // `useCallback` here to memoize the function for subsequent renders.
-  const renderElement = React.useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
-
-const renderMark = React.useCallback((props: CustomMarkProps) => {
-  switch (props.mark.type) {
-    case 'bold': {
-      return <BoldMark {...props} />
-    }
-    default:
-      return <span {...props} />
-  }
-}, [])
+  const [value, setValue] = React.useState<CustomElement[]>(initialValue)
+  const editor = React.useMemo(() => withReact(createEditor() as ReactEditor), [])
 
   return (
     <div className="app">
       <header>
-        Slate@Next Tester 01
+        <h1>Slate@Next Tester 01</h1>
       </header>
       <main>
         <Slate
           editor={editor}
-          defaultValue={defaultValue}
-          onChange={(value: SlateValue) => {
-            const slateValue = JSON.stringify(value, null, '  ')
-            const serializedValue = serialize(value)
-
-            console.log(slateValue)
-            console.log(serializedValue)
+          value={value}
+          onChange={value => {
+            setValue(value as CustomElement[])
+            const isAstChange = editor.operations.some(
+              op => 'set_selection' !== op.type
+            )
+            if (isAstChange) {
+              // Save the value to Local Storage.
+              const content = JSON.stringify(value)
+              localStorage.setItem('content', content)
+              localStorage.setItem('serializedContent', serialize(value as CustomElement[]))
+            }
           }}
         >
+          <div>
+            <button
+              onMouseDown={event => {
+                event.preventDefault()
+                CustomEditor.toggleBoldMark(editor)
+              }}
+            >
+              Bold
+            </button>
+            <button
+              onMouseDown={event => {
+                event.preventDefault()
+                CustomEditor.toggleCodeBlock(editor)
+              }}
+            >
+              Code Block
+            </button>
+          </div>
           <Editable
             renderElement={renderElement}
-            renderMark={renderMark}
-            onKeyDown={(event: any) => {
+            renderLeaf={renderLeaf}
+            onKeyDown={event => {
+              if (event.key === '&') {
+                // Prevent the ampersand character from being inserted.
+                event.preventDefault()
+                // Execute the `insertText` method when the event occurs.
+                editor.insertText('and')
+              }
+
               if (!event.ctrlKey) {
                 return
               }
 
-              // When "`" is pressed, keep our existing code block logic.
               switch (event.key) {
                 case '`': {
                   event.preventDefault()
-                  const { selection } = editor
-                  const isCode = selection
-                    ? Editor.match(editor, selection, { type: 'code' })
-                    : false
-
-                  Editor.setNodes(
-                    editor,
-                    { type: isCode ? null : 'code' },
-                    { match: 'block' }
-                  )
+                  CustomEditor.toggleCodeBlock(editor)
                   break
                 }
-
-                // When "B" is pressed, add a bold mark to the text.
                 case 'b': {
                   event.preventDefault()
-                  Editor.addMarks(editor, [{ type: 'bold' }])
+                  CustomEditor.toggleBoldMark(editor)
                   break
                 }
               }
             }}
-            onDOMBeforeInput={() => { }}
-
           />
         </Slate>
-
-        asdf
       </main>
     </div>
   )
@@ -129,8 +188,30 @@ const DefaultElement: React.FC<any> = props => {
   return <p {...props.attributes}>{props.children}</p>
 }
 
-const BoldMark: React.FC<any> = props => {
-  return <strong {...props.attributes}>{props.children}</strong>
+const renderElement = (props: any) => {
+  switch (props.element.type) {
+    case 'code':
+      return <CodeElement {...props} />
+    default:
+      return <DefaultElement {...props} />
+  }
+}
+
+// Define a React component to render leaves with bold text.
+const Leaf: React.FC<any> = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
+
+// Define a leaf rendering function that is memoized with `useCallback`.
+const renderLeaf = (props: any) => {
+  return <Leaf {...props} />
 }
 
 export default App
